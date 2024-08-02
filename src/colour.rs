370632e6e62181f5274d96fe0c5578f443531355
snubwoody <wakunguma13@gmail.com>
2024-08-02 10:57:06 +0200

    moved map function to utils

--- src/colour.rs
+++ src/colour.rs
@@ -1,16 +1,24 @@
-use super::map;
+use crate::utils::map;
 
-pub fn rgb(r:i32,g:i32,b:i32) -> [f32;4]{
+pub fn rgb(r:u8,g:u8,b:u8) -> [f32;4]{
 	let red = map(r as f32, [0.0,255.0], [0.0,1.0]);
 	let green = map(g as f32, [0.0,255.0], [0.0,1.0]);
 	let blue = map(b as f32, [0.0,255.0], [0.0,1.0]);
 	return [red,green,blue,1.0]
 }
 
-pub fn rgba(r:i32,g:i32,b:i32,a:i32) -> [f32;4]{
+pub fn rgba(r:u8,g:u8,b:u8,a:u8) -> [f32;4]{
 	let red = map(r as f32, [0.0,255.0], [0.0,1.0]);
 	let green = map(g as f32, [0.0,255.0], [0.0,1.0]);
 	let blue = map(b as f32, [0.0,255.0], [0.0,1.0]);
 	let alpha = map(a as f32, [0.0,100.0], [0.0,1.0]);
 	return [red,green,blue,alpha]
 }
+
+
+/* // TODO
+enum Colour{
+	Hex(String),
+	Rgb(u8,u8,u8),
+	Rgba(u8,u8,u8,u8),
+} */
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod widgets;
 mod view;
 mod colour;
 mod app;
+pub mod utils;
 pub mod surface;
 pub mod text;
 pub mod vertex;
@@ -45,20 +46,5 @@ fn run_app() {
 		.add_view(page);
 
 	app.run()
-}
-
-/// Map value from one range to another. Any overflow is clipped to the min or max
-fn map(mut value:f32,input_range:[f32;2],output_range:[f32;2]) -> f32{
-	if value > input_range[1]{
-		value = input_range[1]
-	}
-	else if value < input_range[0] {
-		value = input_range[0]
-	}
-
-	let scale = (output_range[1] - output_range[0]) / (input_range[1] - input_range[0]);
-	let offset = input_range[0]*(scale)+output_range[0];
-
-	return  value * scale + offset;
 }
 
