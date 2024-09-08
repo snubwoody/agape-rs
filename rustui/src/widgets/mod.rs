9fc15266b2441f8b57d47ae951d3626246288bbb
snubwoody <wakunguma13@gmail.com>
2024-09-08 07:36:36 -0600

    reverted back to a tree structure

--- rustui/src/widgets/mod.rs
+++ rustui/src/widgets/mod.rs
@@ -55,7 +55,7 @@ impl WidgetBody {
 		context:&RenderContext,
 	) {
 		// Arrange the children
-		//self.arrange_widgets();
+		self.arrange_widgets();
 
 		// Render the parent then the children
 		self.surface.draw(display, frame, window, context);
@@ -63,13 +63,12 @@ impl WidgetBody {
 	}
 
 	/// TODO
-	pub fn arrange_widgets(&mut self,children:&mut Vec<Box<WidgetBody>>) {
+	pub fn arrange_widgets(&mut self) {
 		// Arrange the children
 		let position = self.surface.get_position();
 		self.children.iter_mut().for_each(|widget| {
-			//widget.arrange_widgets();
-		}
-		);
+			widget.arrange_widgets();
+		});
 
 		let size = self.layout.arrange([position.0,position.1],&mut self.children);
 		self.surface.size(size.0 as f32, size.1 as f32);
@@ -106,7 +105,7 @@ pub struct WidgetNode{
 /// Central structure that holds all the [`Widget`]'s, this is 
 /// where rendering, layouts and events are processed from.
 pub struct WidgetTree{
-	pub widgets:Vec<WidgetNode>,
+	pub widgets:Vec<WidgetBody>,
 }
 
 impl WidgetTree where  {
@@ -124,50 +123,16 @@ impl WidgetTree where  {
 		parent_id:Option<usize>,
 		children:Vec<usize>
 	) {
-		let node = WidgetNode{
-			body:widget.build(),
-			id,
-			parent:parent_id,
-			children
-		};
 
-		self.widgets.push(node);
+		self.widgets.push(widget.build());
 	}
 
-	fn get_widget(&self,id:usize) -> Option<&WidgetNode>{
-		for (_,node) in self.widgets.iter().enumerate() {
-			if node.id == id {
-				return Some(node)
-			}
-		}
 
-		None
-	}
-
-	fn walk_reverse(&self){
-		let proposed_sizes:HashMap<usize,(u32,u32)> = HashMap::new();
-
-		// Walk the tree in reverse
-		for (_,node) in self.widgets.iter().rev().enumerate(){
-			match node.body.layout{
-				Layout::SingleChild { width, height } => {
-					dbg!(node.body.surface.get_size());
-				},
-				_ => {}
-			}
-		}
+	fn walk(&self){
+	
 	}
 
 	fn arrange_layouts(&self){
-		for (_,node) in self.widgets.iter().enumerate(){
-			dbg!(&node.body.constraint);
-			match node.body.layout{
-				Layout::Horizontal { spacing, padding } => {
-
-				}
-				_ => {}
-			}
-		}
 	}
 
 	/// Draw the [`WidgetTree`] to the screen.
@@ -178,10 +143,8 @@ impl WidgetTree where  {
 		window:&Window,
 		context:&RenderContext,
 	) {
-		self.walk_reverse();
-
 		self.widgets.iter_mut().for_each(|widget| {
-			widget.body.render(display, frame, window, context);
+			widget.render(display, frame, window, context);
 		})
 	}
 }
