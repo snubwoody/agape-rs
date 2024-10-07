40261543cb34c74f5a6888e5ed57e90df3aec116
waku <wakunguma13@gmail.com>
2024-10-07 13:31:20 -0600

    added state struct

--- renderer/src/main.rs
+++ renderer/src/main.rs
@@ -5,17 +5,25 @@ use winit::{
     window::{Window, WindowBuilder},
 };
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
-    event_loop.run(move |event, control_flow| match event {
+	let mut state = State::new(&window).await;
+
+    event_loop.run(move |event, control_flow| {
+		println!("Jey");
+		match event {
         Event::WindowEvent {ref event,window_id,} => match event {
             WindowEvent::CloseRequested => control_flow.exit(),
+			WindowEvent::Resized(size) => state.resize(*size),
+			WindowEvent::RedrawRequested => state.render().unwrap(),
             _ => {}
         },
         _ => {}
-    }).unwrap();
+	    }
+	}).unwrap();
 }
 
 struct State<'a>{
@@ -28,4 +36,112 @@ struct State<'a>{
     // it gets dropped after it as the surface contains
     // unsafe references to the window's resources.
     window: &'a Window,
+}
+
+impl<'a> State<'a> {
+	async fn new(window:&'a Window) -> Self{
+		let size = window.inner_size();
+
+		let instance = wgpu::Instance::new(wgpu::InstanceDescriptor{
+			backends: wgpu::Backends::PRIMARY,
+			..Default::default()
+		});
+
+		// Create the surface to draw on
+		let surface = instance.create_surface(window).unwrap();
+
+		let adapter = instance.request_adapter(&wgpu::RequestAdapterOptions{
+			power_preference: Default::default(),
+			compatible_surface: Some(&surface),
+			force_fallback_adapter:false
+		}).await.unwrap();
+
+		let (device,queue) = adapter.request_device(&wgpu::DeviceDescriptor{
+			required_features: wgpu::Features::empty(),
+			required_limits:wgpu::Limits::default(),
+			label: None,
+			memory_hints: Default::default()
+		}, None).await.unwrap();
+
+		let surface_caps = surface.get_capabilities(&adapter);
+
+		let surface_format = surface_caps
+			.formats
+			.iter()
+			.find(|f|f.is_srgb())
+			.copied()
+			.unwrap_or(surface_caps.formats[0]);
+
+		let config = wgpu::SurfaceConfiguration{
+			usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+			format: surface_format,
+			width: size.width,
+			height: size.height,
+			present_mode: surface_caps.present_modes[0],
+			alpha_mode:surface_caps.alpha_modes[0],
+			view_formats: vec![],
+			desired_maximum_frame_latency: 2
+		};
+
+		Self{
+			surface,
+			device,
+			queue,
+			config,
+			size,
+			window
+		}
+	}
+
+	fn resize(&mut self, new_size:winit::dpi::PhysicalSize<u32>){
+		if new_size.width > 0 && new_size.height > 0 {
+			self.size = new_size;
+			self.config.width = new_size.width;
+			self.config.height = new_size.height;
+			self.surface.configure(&self.device, &self.config);
+		}
+	}
+
+	fn update(&mut self){
+		todo!()
+	}
+
+	fn render(&mut self) -> Result<(),wgpu::SurfaceError> {
+		let output = self.surface.get_current_texture()?;
+		let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+		let mut encoder = self.device.create_command_encoder(&wgpu::CommandEncoderDescriptor{
+			label:Some("Render encoder")
+		});
+
+		let render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+			label: Some("Render Pass"),
+			color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+				view: &view,
+				resolve_target: None,
+				ops: wgpu::Operations {
+					load: wgpu::LoadOp::Clear(wgpu::Color {
+						r: 0.1,
+						g: 0.2,
+						b: 0.3,
+						a: 1.0,
+					}),
+					store: wgpu::StoreOp::Store,
+				},
+			})],
+			depth_stencil_attachment: None,
+			occlusion_query_set: None,
+			timestamp_writes: None,
+		});
+
+		// Drop the render pass because it borrows encoder
+		// mutably
+		drop(render_pass);
+	
+		// submit will accept anything that implements IntoIter
+		self.queue.submit(std::iter::once(encoder.finish()));
+		output.present();
+
+		Ok(())
+	}
 }
