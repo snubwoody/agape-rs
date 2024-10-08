a5dc87b1fef9cb73be97a18d97adcb31d85f050c
waku <wakunguma13@gmail.com>
2024-10-08 09:15:25 -0600

    got a triangle rendering to the screen

--- renderer/src/main.rs
+++ renderer/src/main.rs
@@ -13,7 +13,6 @@ async fn main() {
 	let mut state = State::new(&window).await;
 
     event_loop.run(move |event, control_flow| {
-		println!("Jey");
 		match event {
         Event::WindowEvent {ref event,window_id,} => match event {
             WindowEvent::CloseRequested => control_flow.exit(),
@@ -32,10 +31,8 @@ struct State<'a>{
 	queue: wgpu::Queue,
 	config: wgpu::SurfaceConfiguration,
 	size: winit::dpi::PhysicalSize<u32>,
-	// The window must be declared after the surface so
-    // it gets dropped after it as the surface contains
-    // unsafe references to the window's resources.
     window: &'a Window,
+	render_pipeline: wgpu::RenderPipeline
 }
 
 impl<'a> State<'a> {
@@ -83,13 +80,64 @@ impl<'a> State<'a> {
 			desired_maximum_frame_latency: 2
 		};
 
+		let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor { 
+			label: Some("Shader module"), 
+			source: wgpu::ShaderSource::Wgsl(include_str!("../shaders/shader.wgsl").into())
+		});
+
+		let render_pipeline_layout = 
+			device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor { 
+				label: Some("Render pipeline layout"), 
+				bind_group_layouts: &[], 
+				push_constant_ranges: &[] 
+			});
+
+		let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor { 
+			label: Some("Render pipeline"), 
+			layout: Some(&render_pipeline_layout), 
+			vertex: wgpu::VertexState { 
+				module: &shader, 
+				entry_point: "vs_main", 
+				compilation_options: Default::default(), 
+				buffers: &[] 
+			}, 
+			fragment: Some(wgpu::FragmentState{
+				module:&shader,
+				entry_point:"fs_main",
+				compilation_options: Default::default(),
+				targets:&[Some(wgpu::ColorTargetState { 
+					format: config.format, 
+					blend: Some(wgpu::BlendState::ALPHA_BLENDING), 
+					write_mask: wgpu::ColorWrites::ALL 
+				})]
+			}), 
+			primitive: wgpu::PrimitiveState { 
+				topology: wgpu::PrimitiveTopology::TriangleList, 
+				strip_index_format: None, 
+				front_face: wgpu::FrontFace::Ccw, 
+				cull_mode: Some(wgpu::Face::Back), 
+				unclipped_depth: false, 
+				polygon_mode: wgpu::PolygonMode::Fill, 
+				conservative: false 
+			}, 
+			multisample: wgpu::MultisampleState { 
+				count: 1, 
+				mask: !0, 
+				alpha_to_coverage_enabled: false, 
+			}, 
+			multiview: None, 
+			cache: None, 
+			depth_stencil: None, 
+		});
+
 		Self{
 			surface,
 			device,
 			queue,
 			config,
 			size,
-			window
+			window,
+			render_pipeline
 		}
 	}
 
@@ -114,7 +162,7 @@ impl<'a> State<'a> {
 			label:Some("Render encoder")
 		});
 
-		let render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+		let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
 			label: Some("Render Pass"),
 			color_attachments: &[Some(wgpu::RenderPassColorAttachment {
 				view: &view,
@@ -133,6 +181,9 @@ impl<'a> State<'a> {
 			occlusion_query_set: None,
 			timestamp_writes: None,
 		});
+
+		render_pass.set_pipeline(&self.render_pipeline);
+		render_pass.draw(0..3, 0..1);
 
 		// Drop the render pass because it borrows encoder
 		// mutably
