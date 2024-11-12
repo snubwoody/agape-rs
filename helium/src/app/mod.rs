7fa830527749ec01f1ec5160893e2d64ba8d0106
waku <wakunguma13@gmail.com>
2024-11-12 03:53:57 -0700

    fixed text stretching issue

--- helium/src/app/mod.rs
+++ helium/src/app/mod.rs
@@ -150,6 +150,11 @@ impl<'a> AppState<'a> {
             0,
             bytemuck::cast_slice(&[self.size.width, self.size.height]),
         );
+        self.queue.write_buffer(
+            &self.context.text_renderer.window_buffer,
+            0,
+            bytemuck::cast_slice(&[self.size.width, self.size.height]),
+        );
     }
 }
 
