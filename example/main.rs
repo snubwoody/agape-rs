f1548fd25ed17a75d83f1a18edb157f39f404d54
charlemagne <67170475+snubwoody@users.noreply.github.com>
2024-12-02 22:01:05 -0700

    Revert "Improved layout system"

--- example/main.rs
+++ example/main.rs
@@ -11,7 +11,8 @@ fn app(){
 	let color = hex!("#afffff");
 	
 	let hstack = hstack![
-		Rect::new(200.0, 200.0, color.clone()),
+		Text::new("Hello wo"),
+		Text::new("1"),
 		Rect::new(200.0,200.0,color)
 	].spacing(54).padding(24);
     
