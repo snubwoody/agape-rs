b10d8e90313d321b74e7009255c7c44cace37c73
waku <wakunguma13@gmail.com>
2025-02-07 17:40:52 -0700

    added examples/scroll

---
+++ examples/scroll.rs
@@ -0,0 +1,20 @@
+use helium::{vstack, widgets::Rect, App, BLACK};
+
+#[tokio::main]
+async fn main() -> Result<(),helium::Error>{
+	// TODO Overflowing widgets will need to be clipped
+	let list = vstack! {
+		Rect::new(200.0, 200.0).color(BLACK),	
+		Rect::new(200.0, 200.0).color(BLACK),	
+		Rect::new(200.0, 200.0).color(BLACK),	
+		Rect::new(200.0, 200.0).color(BLACK),	
+		Rect::new(200.0, 200.0).color(BLACK),	
+	}
+	.spacing(24)
+	.fill()
+	.align_center();
+
+	let mut app = App::new();
+	app.add_page(list);
+	app.run().await
+}
