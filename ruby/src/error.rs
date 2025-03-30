a2c6f6b722e329b68e639db05f0dcc8b433db5ee
waku <wakunguma13@gmail.com>
2025-03-30 16:06:41 -0600

    feat(ruby): add from impl of winit's `EventLoopError` and `OsError`

--- ruby/src/error.rs
+++ ruby/src/error.rs
@@ -2,11 +2,15 @@ use thiserror::Error;
 
 pub type Result<T> = std::result::Result<T,Error>;
 
-#[derive(Debug, Clone, Error, PartialEq, Eq)]
+#[derive(Debug, Error,)]
 pub enum Error {
     /// A resource was not found at it's expected location.  
     #[error("Resource not found:{0}")]
     ResourceNotFound(String),
+	#[error(transparent)]
+	EventLoopError(#[from] winit::error::EventLoopError),
+	#[error(transparent)]
+	OsError(#[from] winit::error::OsError),
 }
 
 impl Error {
