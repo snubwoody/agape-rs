a8a8265dd0c69e0c98406a80cf39f3fef48d5f40
waku <wakunguma13@gmail.com>
2025-03-30 16:50:13 -0600

    revert: "refactor(ruby): export `helium_core` types"
    This reverts commit b7d95001bcb33f60111b8dc3a7fdccb2fe234a6a.

--- examples/renderer.rs
+++ examples/renderer.rs
@@ -1,5 +1,6 @@
-use helium::{colors::{BLACK, BLUE, RED},};
-use ruby::Color;
+use helium::colors::BLACK;
+use ruby::RectSurface;
+
 
 #[tokio::main]
 async fn main() -> ruby::Result<()> {
@@ -11,11 +12,7 @@ async fn main() -> ruby::Result<()> {
 	let app = ruby::App::new()?;
 
 	app.run(|r|{
-		r.draw_square(250.0, BLUE, 24.0);
-		r.draw_square(250.0, BLUE, 24.0);
-		r.draw_square(550.0, Color::rgb(24, 240, 120), 24.0);
-		r.draw_square(50.0, BLACK, 24.0);
-		r.draw_square(50.0, RED, 24.0);
+		r.draw([RectSurface::new(300.0, 200.0).color(BLACK)]);
 	}).await?;
 
 	Ok(())
