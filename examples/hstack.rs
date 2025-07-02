7783d86602377e21a881d506f14fc3b23614f029
waku <wakunguma13@gmail.com>
2025-07-02 12:16:09 +0200

    Add layout system for solving the layout tree

--- examples/hstack.rs
+++ examples/hstack.rs
@@ -5,9 +5,6 @@ use helium::{
 };
 
 fn main() -> Result<(), helium::Error> {
-    let _ = dotenv();
-    env_logger::init();
-
     let hstack = hstack! {
         Rect::new(200.0, 200.0).color(Color::BLACK),
         Rect::new(200.0, 200.0).color(Color::BLACK),
