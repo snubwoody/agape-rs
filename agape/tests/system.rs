bae804f977a37ece7cd4e220496c5c951b23fbaa
waku <wakunguma13@gmail.com>
2025-07-06 23:47:58 +0200

    Implement `IntoSystem` for event systems

--- agape/tests/system.rs
+++ agape/tests/system.rs
@@ -1,8 +1,8 @@
 use agape::{
-    Resources,
-    hstack,
+    Resources, hstack,
     system::{IntoSystem,System}
 };
+use agape::resources::EventQueue;
 use agape::widgets::Widget;
 use agape_core::{Position, Size};
 use agape_layout::Layout;
@@ -26,6 +26,7 @@ fn get_trait_from_resource(){
 
 #[test]
 fn function_system() {
+    let event_queue = EventQueue::new();
     let mut resources = Resources::new();
     resources.insert(Position::unit(20.0));
 
@@ -33,6 +34,37 @@ fn function_system() {
         resources.get::<Position>().unwrap();
     };
     let mut system = func.into_system();
-    system.run(&mut resources);
+    system.run(&mut resources,&event_queue);
 }
 
+#[test]
+fn event_system() {
+    #[derive(PartialEq,Debug)]
+    struct Dummy;
+
+    let mut event_queue = EventQueue::new();
+    event_queue.push(Dummy);
+
+    let mut resources = Resources::new();
+    resources.insert(Position::unit(20.0));
+
+    let func = |_: &mut Resources,event: &Dummy| {
+        assert_eq!(event,&Dummy)
+    };
+
+    let mut system = func.into_system();
+    system.run(&mut resources,&event_queue);
+}
+
+#[test]
+fn event_system_only_runs_when_present(){
+    let event_queue = EventQueue::new();
+    let mut resources = Resources::new();
+    resources.insert(Position::unit(20.0));
+
+    let func = |_resources: &mut Resources,_event: &()| {
+        panic!();
+    };
+    let mut system = func.into_system();
+    system.run(&mut resources,&event_queue);
+}
