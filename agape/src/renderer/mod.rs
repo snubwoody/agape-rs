a954e4cf0b3a977e7257c5056faca16539672785
waku <wakunguma13@gmail.com>
2025-07-29 10:31:53 -0600

    Change renderer module into directory

---
+++ agape/src/renderer/mod.rs
@@ -0,0 +1,12 @@
+mod image;
+mod rect;
+mod text;
+
+use fontdue::{Font, FontSettings};
+pub use rect::draw_rect;
+pub use text::{draw_text, text_size};
+
+pub fn init_font() -> Font {
+    let bytes = include_bytes!("../../fonts/Inter/static/Inter-Regular.ttf") as &[u8];
+    Font::from_bytes(bytes, FontSettings::default()).unwrap()
+}
