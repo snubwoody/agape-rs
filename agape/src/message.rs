73f5ff4f9f85235adaef50cda26904c7daa1ca2a
waku <wakunguma13@gmail.com>
2025-09-03 11:18:39 -0600

    refactor: Replace `Any` with `Message` trait in `MessageQueue`

--- agape/src/message.rs
+++ agape/src/message.rs
@@ -10,14 +10,16 @@ pub struct MouseButtonDown;
 /// Emitted when the left mouse button is pressed.
 pub struct MouseButtonUp;
 
-pub trait Message: Any + Send + Sync {
-    fn as_any(&self) -> &dyn Any;
-}
+// Marker trait
+/// The message trait is implemented for anything which implements
+/// `Any + Send + Sync`.
+pub trait Message: Any + Send + Sync {}
 
-// TODO: replace Any with Message
-#[derive(Default, Resource, Debug)]
+impl<T: Any + Send + Sync> Message for T {}
+
+#[derive(Default, Resource)]
 pub struct MessageQueue {
-    items: Vec<Box<dyn Any + Send + Sync>>,
+    items: Vec<Box<dyn Message>>,
     frame_count: u32,
 }
 
@@ -30,30 +32,36 @@ impl MessageQueue {
         self.frame_count += 1;
     }
 
-    pub fn has<T: 'static>(&self) -> bool {
-        self.get::<T>().is_some()
+    pub fn has<M: Message>(&self) -> bool {
+        self.get::<M>().is_some()
     }
 
-    pub fn add<T: 'static + Send + Sync>(&mut self, item: T) {
+    pub fn add<M: Message>(&mut self, item: M) {
         // Don't insert the same resource twice
-        if self.get::<T>().is_none() {
+        if self.get::<M>().is_none() {
             self.items.push(Box::new(item));
         }
     }
 
-    pub fn set<T: 'static + Send + Sync>(&mut self, item: T) {
-        self.remove::<T>();
+    pub fn set<M: Message>(&mut self, item: M) {
+        self.remove::<M>();
         self.items.push(Box::new(item));
     }
 
-    pub fn remove<T: 'static>(&mut self) -> Option<T> {
-        let index = self.items.iter().position(|i| i.is::<T>())?;
-        let item = self.items.swap_remove(index);
-        Some(*item.downcast().unwrap())
+    /// Remove and return a message of type `M` from the queue.
+    pub fn remove<M: 'static>(&mut self) -> Option<M> {
+        let index = self
+            .items
+            .iter()
+            .map(|i| i.as_ref() as &dyn Any)
+            .position(|i| i.is::<M>())?;
+        let item: Box<dyn Any> = self.items.swap_remove(index);
+        item.downcast().ok().map(|m| *m)
     }
 
     pub fn get<T: 'static>(&self) -> Option<&T> {
         for item in &self.items {
+            let item = item.as_ref() as &dyn Any;
             match item.downcast_ref::<T>() {
                 Some(item) => return Some(item),
                 None => continue,
@@ -72,7 +80,8 @@ impl MessageQueue {
     }
 
     pub fn clear(&mut self) {
-        if self.frame_count > 3 {
+        // TODO: 2 frames might be better
+        if self.frame_count >= 3 {
             self.items.clear();
             self.frame_count = 0;
         }
@@ -84,5 +93,31 @@ pub fn update_cursor_pos(queue: Res<EventQueue>, mut cursor_position: ResMut<Cur
         if let WindowEvent::CursorMoved { position, .. } = event {
             cursor_position.update(Position::from(*position));
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn message_queue_tick() {
+        let mut messages = MessageQueue::new();
+        messages.tick();
+        messages.tick();
+        assert_eq!(messages.frame_count, 2);
+    }
+
+    #[test]
+    fn clear_messages() {
+        let mut messages = MessageQueue::new();
+        messages.add(String::new());
+        messages.tick();
+        messages.tick();
+        messages.clear();
+        assert_eq!(messages.len(), 1);
+        messages.tick();
+        messages.clear();
+        assert!(messages.is_empty());
     }
 }
