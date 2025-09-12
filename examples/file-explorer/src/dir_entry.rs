8be08a7d8a8d028b831436101f779bc24e31388c
waku <wakunguma13@gmail.com>
2025-09-12 12:01:44 -0600

    feat(examples): Add icon to directory entries

--- examples/file-explorer/src/dir_entry.rs
+++ examples/file-explorer/src/dir_entry.rs
@@ -1,5 +1,6 @@
-use agape::MessageQueue;
+use agape::layout::AxisAlignment;
 use agape::widgets::{Container, Svg, Text, View, Widget};
+use agape::{MessageQueue, hstack};
 use std::path::PathBuf;
 
 pub struct DirEntry {
@@ -18,8 +19,14 @@ impl View for DirEntry {
     fn update(&mut self, _: &mut MessageQueue) {}
 
     fn view(&self) -> Box<dyn Widget> {
-        Svg::open("phosphor-icons/regular");
-        let widget = Container::new(Text::new(&self.title)).padding(12);
+        let icon = Svg::open("examples/file-explorer/icons/regular/folder.svg")
+            .unwrap()
+            .fixed(24.0, 24.0);
+
+        let widget = hstack![icon, Text::new(&self.title)]
+            .padding(12)
+            .cross_axis_alignment(AxisAlignment::Center)
+            .spacing(12);
         Box::new(widget)
     }
 }
