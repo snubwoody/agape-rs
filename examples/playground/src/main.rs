e8d749064e463ff0c760b5fb44661bcd3ce9d2d8
waku <wakunguma13@gmail.com>
2025-10-09 02:23:13 -0600

    feat: Add StateCell

--- examples/playground/src/main.rs
+++ examples/playground/src/main.rs
@@ -1,25 +1,31 @@
-use agape::widgets::{HStack, TextField};
-use agape::{App, GlobalId, Widget, hstack};
+use agape::state::StateCell;
+use agape::widgets::{Button, HStack, StatelessWidget, TextField, VStack, *};
+use agape::{App, GlobalId, Widget, hstack, vstack};
+use std::cell::{Cell, RefCell};
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 
 fn main() -> agape::Result<()> {
     tracing_subscriber::fmt::init();
-    App::new(Main::new()).run()
+    App::new(Page::default()).run()
 }
 
-#[derive(Default, Widget)]
-struct Main {
-    id: GlobalId,
-    #[child]
-    widget: HStack,
+#[derive(Default)]
+struct Page {
+    count: StateCell<usize>,
 }
 
-impl Main {
-    pub fn new() -> Self {
-        let widget = hstack![TextField::new()].fill().align_center();
+impl StatelessWidget for Page {
+    type Widget = VStack;
 
-        Self {
-            id: GlobalId::new(),
-            widget,
-        }
+    fn build(&self) -> Self::Widget {
+        let count = self.count.clone();
+        vstack![
+            Text::new(&format!("{}", self.count.get())),
+            Button::text("Click me").on_click(move |_| count.set(|count| count + 1)),
+        ]
+        .spacing(8)
+        .fill()
+        .align_center()
     }
 }
